468942451419ac7b79d875fcefcfb67dd6a2aff2
Ankit Kumar <ankit.616.ap@gmail.com>
2025-11-11 22:05:19 +0530

    python packages list added and code initiated

--- code.py
+++ code.py
@@ -1,1 +1,20 @@
 # this is python code for applying bayesian MMx
+
+# importing libraries
+
+import pandas as pd
+import numpy as np
+import pymc as pm
+import arviz as az
+import matplotlib.pyplot as plt
+
+# Loading the channels cost/engagement and prescription data
+
+path = r'C:\D\MMX\Supernus\November 25\Overall'
+df_eng = pd.read_csv(path+"\cost_overall.csv")
+df = df_eng.copy()
+print(df.head())
+df_cols = df.columns.tolist()
+print(df_cols)
+channels = df_cols[1:-1]
+channels
